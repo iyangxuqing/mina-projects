8eab2d452f63ec7a4fceb1fae909e4a80d4b5e09
iyangxuqing@qq.com <iyangxuqing@qq.com>
2017-04-24 17:51:59 +0800

    articles

---
+++ cizhuang/ceshi/notice/notice.data.js
@@ -0,0 +1,127 @@
+export var style = {
+    'create-time': [
+        'font-weight: 500;',
+        'font-size: 1em;',
+        'color: #666;',
+    ],
+    'foot-note': [
+        'font-weight: 500;',
+        'font-size: 0.9em;',
+        'color: #666;',
+        'text-align: center;',
+        'padding-top: 20rpx;',
+        'margin-top: 120rpx;',
+        'border-top: 1px solid #ccc;',
+    ],
+    'sub-title': [
+        'line-height: 2.6;',
+        'text-align: center;',
+        'background-color: khaki;',
+        'margin-top: 60rpx;',
+    ]
+}
+
+export var articles = []
+articles[0] = {
+    groupId: 1,
+    id: 101,
+    time: '',
+    title: '你没看错，现金红包真的免费领',
+    image: 'wode/notice/s04.jpg',
+    summary: '',
+    contents: [
+        {
+            title: '4.02，疯抢东鹏，低至39.8元',
+        },
+        {
+            title: '2017-03-20',
+            style: 'create-time'
+        },
+        {
+            subTitle: '3月28日-4月1日',
+            style: 'sub-title'
+        },
+        {
+            texts: [
+                '活动前奏，优惠券提前领'
+            ]
+        },
+        {
+            image: 'wode/notice/s06.jpg'
+        },
+        {
+            texts: [
+                '还有仿古砖提前预订，早订更实惠。'
+            ]
+        },
+        {
+            image: 'wode/notice/s07.jpg'
+        },
+        {
+            texts: [
+                '别说小编没有给不需要仿古砖的MM和GG们谋福利，这几天提前交定金，活动正式开始的时候以1抵2，最多可交500元定金，这可是实实在在的福利哦，名额有限，大家抓紧了哈。（定金不退）。'
+            ]
+        },
+        {
+            image: 'wode/notice/s08.jpg'
+        },
+        {
+            subTitle: '4月2日-4月5日',
+            style: 'sub-title'
+        },
+        {
+            texts: [
+                '优惠一：订金在今天可以1元当两元使用，没有最低消费额度，也就是说您交了200元的定金，可以直接选购400元的产品。'
+            ]
+        },
+        {
+            image: 'wode/notice/s09.jpg'
+        },
+        {
+            texts: [
+                '优惠二： 满1000减50元，满2000减100元，满9999减500元，买的多减的多。（不可重复优惠）。'
+            ],
+        },
+        {
+            image: 'wode/notice/s10.jpg'
+        },
+        {
+            texts: [
+                '优惠三： 凡是这三天在东鹏瓷砖店购买满2000以上者可获得抽奖券一张，4000元送两张抽奖券，上不封顶（奖券百分百中奖，最大奖免单）.您还在等什么。'
+            ]
+        },
+        {
+            image: 'wode/notice/s11.jpg'
+        },
+        {
+            title: '↑↑↑以上活动最终解释权归东鹏所有↑↑↑',
+            style: 'foot-note'
+        }
+    ]
+}
+
+articles[1] = {
+    groupId: 1,
+    id: 102,
+    title: '3.18-3.21 义乌家具博览会，我们东鹏在等你。',
+    image: 'wode/notice/s03.jpg',
+    contents: []
+}
+
+articles[2] = {
+    groupId: 1,
+    id: 103,
+    title: '端午纳福！满4999元减200，满9888元减500。',
+    image: 'wode/notice/s02.jpg',
+    contents: []
+}
+
+articles[3] = {
+    groupId: 2,
+    id: 201,
+    time: '2017年3月20日',
+    title: '4.02，疯抢东鹏，低至39.8元',
+    image: 'wode/notice/s05.jpg',
+    summary: '抢全屋墙面哇藻泥、部分产品低至1.1折，增值不断 100=3000',
+    contents: []
+}
